dc0d1c1d3e839b9061df77e864ef7271dcd20760
Alex Canady <mcc9513@gmail.com>
2024-09-14 20:57:44 -0400

    start

---
+++ src/main/java/org/example/ordermangementsystem/OrderMangementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.ordermangementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMangementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMangementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ordermangementsystem/OrderMangementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ordermangementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMangementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
